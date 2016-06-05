957468a1beac0976af5a8cd965e7e1e210b0a9b5
viktornord1 <viktornord1@gmail.com>
2016-06-05 22:52:26 +0300

    implemented editable component to edit product data

--- src/components/product/product.component.ts
+++ src/components/product/product.component.ts
@@ -2,21 +2,19 @@ import {Component, Input, OnInit, Output} from '@angular/core';
 import {MdCard} from '@angular2-material/card';
 import {MdSlideToggle} from '@angular2-material/slide-toggle';
 import {MdButton} from '@angular2-material/button';
+import {EditableComponent} from 'src/components/editable/editable.component';
 
 const template = require('./product.tpl.html') as string;
 
 @Component({
     selector: 'warehouse-product',
     template,
-    directives: [ProductComponent, MdCard, MdButton, MdSlideToggle]
+    directives: [ProductComponent, MdCard, MdButton, MdSlideToggle, EditableComponent]
 })
 
-export class ProductComponent implements OnInit {
+export class ProductComponent {
     @Input() product:any;
     expanded:boolean = false;
-
-    ngOnInit():void {
-    }
 
     toggle() {
         this.expanded = !this.expanded;
@@ -37,6 +35,20 @@ export class ProductComponent implements OnInit {
         return !this.product.children ?
             this.product.weight :
             _.map(product.children, 'weight').reduce((totalWeight:number, weight:number) => totalWeight + weight, 0) as number;
+    }
+
+    updateWeight(newWeight:string) {
+        const weight:number = Number(newWeight);
+        !Number.isNaN(weight) && (this.product.weight = weight);
+    }
+
+    updateTitle(newTitle:string) {
+        this.product.title = newTitle;
+    }
+
+    hasRelatedProducts() {
+
+        return !_.isEmpty(this.product.children);
     }
 
 
