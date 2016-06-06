6f25f383dfd80b902e822715bce8ff48c8038aa8
viktornord1 <viktornord1@gmail.com>
2016-06-07 00:52:04 +0300

    tiny clean up

--- src/components/product/product.component.ts
+++ src/components/product/product.component.ts
@@ -1,9 +1,7 @@
 import {Component, Input, OnInit, Output} from '@angular/core';
-import {MdDialog} from 'ng2-material';
+import {MdDialog, MdButton, MdIcon} from 'ng2-material';
 import {MdCard} from '@angular2-material/card';
 import {MdSlideToggle} from '@angular2-material/slide-toggle';
-import {MdButton} from '@angular2-material/button';
-import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 import {EditableComponent} from 'src/components/editable/editable.component';
 import {Product} from './product-model';
 import IProduct = warehouse.IProduct;
@@ -13,8 +11,7 @@ const template = require('./product.tpl.html') as string;
 @Component({
     selector: 'warehouse-product',
     template,
-    directives: [ProductComponent, MdCard, MdButton, MdIcon, MdSlideToggle, MdDialog, EditableComponent],
-    viewProviders: [MdIconRegistry]
+    directives: [ProductComponent, MdCard, MdButton, MdIcon, MdSlideToggle, MdDialog, EditableComponent]
 })
 
 export class ProductComponent implements OnInit {
